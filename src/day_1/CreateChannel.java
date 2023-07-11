eeb48b10da621addda37421a0c05869ea07f4a41
Mesutbaki07 <mesutbaki07@gmail.com>
2023-07-11 10:01:28 +0300

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,9 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("User is abe to create channels");
+        int x=0; //changed from IntelliJ
+        if(x==0){
+            System.out.println("equal");
+        }
     }
 }
